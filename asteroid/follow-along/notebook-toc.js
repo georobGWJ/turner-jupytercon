39314fcf249717e16bb98d1d65851a57391e13b2
Robert Turner Jr <georob23@gmail.com>
2017-08-23 16:36:42 -0400

    Finalize extension.

--- asteroid/follow-along/notebook-toc.js
+++ asteroid/follow-along/notebook-toc.js
@@ -1,6 +1,36 @@
 define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils, $)
 {
-    function create_toc_text() {}
+    function create_toc_text() {
+      var toc_text = "# Table of Contents\n";
+      // Get an array of cell objects
+      var cells = Jupyter.notebook.get_cells();
+      // Track how many heading cells there are
+      var heading_count = 0;
+      // Loop through cells to find headers
+      for (var i = 0; i < cells.length; i++) {
+        var cell = cells[i];
+        if (cell.cell_type === 'markdown') {
+          var cell_content = cell.get_text();
+          if (cell_content.startsWith'#') {
+            heading_count++;
+            var last_hash_idx = cell_content.lastIndexOf('#'); // Grabs test starting after last '#'
+            // SEE LINES 18 to 21 OF IMPLEMENTED CODE FOR a FIX!!!
+            var hash_num = last_hash_idx + 1;
+            var heading_title = cell_content.substr(hash_num);
+            // Now create Link to heading
+            var link_fragment_heading = heading_title.toLowerCase().replace(" ","_");
+            // Currently we need to do links via HTML
+            var link_fragment = '<a name="' + link_fragment_heading + '"></a>';
+            cell.set_text(cell.get_text() + link_fragment);
+            cell.render();    // refresh cell
+            // Append to String of Header links
+            toc_text += (Array(hash_num).join(' ') + '-');
+            toc_text += '[' + heading_title + '](#' + link_fragment_heading + ') \n';
+          }
+        }
+      }
+      return toc_text;
+    }
 
     function load_toc_cell(toc_cell, toc_text) {
       if (toc_cell === null) {
@@ -9,7 +39,9 @@ define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils
         var toc_cell = Jupyter.notebook.insert_cell_at_index('markdown', 0);
       }
 
+      // Add text to cell
       toc_cell.set_text(toc_text);
+      // Show the cell
       toc_cell.render();
     }
 
@@ -19,7 +51,8 @@ define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils
     }
 
     function refresh_toc() {
-
+      var toc_text = create_toc_text();
+      load_toc_cell(Jupyter.notebook.get_cell(0), toc_text);
     }
 
     function place_toc_button() {
